fa15a4907cf5ed1bf5946fbe3b5a52d593b07877
bavicente9 <bavicente98@gmail.com>
2021-10-19 08:52:58 -0500

    readme

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          oeauaoeuoea
+          euieui
+          ui
+          
         </p>
         <a
           className="App-link"
