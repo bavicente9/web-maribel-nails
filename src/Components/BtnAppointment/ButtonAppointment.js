cb76b2cd453b1df3aba44584392211759a650feb
bavicente9 <bavicente98@gmail.com>
2021-11-17 06:24:14 -0500

    Appointment Button function

--- src/Components/BtnAppointment/ButtonAppointment.js
+++ src/Components/BtnAppointment/ButtonAppointment.js
@@ -3,8 +3,14 @@ import './ButtonAppointment.css'
 
 
 function ButtonAppointment(props) {
+    const link = 'https://booksy.com/es-es/11160_maribel-nail-s_estilistas-unas_52975_getafe'
+    const handleClick = () => {
+        window.open(
+            link, '__blank'
+        )
+    }
     return (
-        <button  className ={`button-Appointment ${props.className}`}  >RESERVAR CITA</button>
+        <button className={`button-Appointment ${props.className}`} onClick = {handleClick}  >RESERVAR CITA</button>
     )
 }
 
