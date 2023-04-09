279f7c118a9a210ebd511e3e75d4e0a631a1868a
Bryan Vicente <bavicente98@gmail.com>
2023-04-09 15:54:13 +0200

    Añadido serivicio y actualizada la descripcion de los otros

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,27 +10,11 @@ function Footer() {
 
                 <div className='logoAndSocialMediaContainer'>
                     <img alt='' src='./images/logo.jpg' />
-                    <SocialMediaButtons className = 'footerSocialMedia' />
                 </div>
+                    <SocialMediaButtons className = 'footerSocialMedia' />
                 <div className='info1'>
                     <p><b>Tlf:</b>{contactInfo.tlf}</p>
                     <p><b>Dirección:</b>{contactInfo.location}</p>
-                </div>
-                <div className='info2'>
-                    <p><b>Horario:</b></p>
-                    <div className = 'schedule'>
-                    {
-                        contactInfo.schedule.map((item, index) => {
-                            return (
-                                <div className='scheduleInfo' key={index} >
-                                    <p><b>{item.time} :</b></p>
-                                    <p>{item.hours}</p>
-                                </div>
-                            )
-                        })
-                    }
-                    </div>
-
                 </div>
             </div>
             <div className = 'translucidLayer'/>
